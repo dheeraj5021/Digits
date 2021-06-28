dfd997c8e0128a99db1bee50dbaea8de5ac1ef96
dheeraj5021 <86486999+dheeraj5021@users.noreply.github.com>
2021-06-28 22:37:11 +0530

    Add files via upload

---
+++ Digits.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Digits
+{
+ public static void main(String args[])
+  {
+    int a[]={1,2,3,4,5};
+    for(int i=1;i<=5;i++)
+     {
+       if(i%2==0)
+        {
+          System.out.println("even digit");
+        }
+       else
+        {
+           System.out.println("odd digit");
+        }
+     }
+  }
+}
